0df09228679e2d2116f9a779f45487085c7b0912
Nikolai Kristiansen <nikolaik@gmail.com>
2015-09-09 22:22:54 +0200

    Minor

--- oliversite/settings.py
+++ oliversite/settings.py
@@ -1,13 +1,4 @@
-"""
-Django settings for oliversite project.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/1.7/topics/settings/
-
-For the full list of settings and their values, see
-https://docs.djangoproject.com/en/1.7/ref/settings/
-"""
-
+from __future__ import unicode_literals
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 import os
 BASE_DIR = os.path.dirname(os.path.dirname(__file__))
@@ -53,13 +44,11 @@ MIDDLEWARE_CLASSES = (
 )
 
 ROOT_URLCONF = 'oliversite.urls'
-
 WSGI_APPLICATION = 'oliversite.wsgi.application'
 
 
 # Database
 # https://docs.djangoproject.com/en/1.7/ref/settings/#databases
-
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
@@ -69,24 +58,18 @@ DATABASES = {
 
 # Internationalization
 # https://docs.djangoproject.com/en/1.7/topics/i18n/
-
 LANGUAGE_CODE = 'en-us'
-
 TIME_ZONE = 'Europe/Oslo'
-
 USE_I18N = True
-
 USE_L10N = True
-
 USE_TZ = True
-
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.7/howto/static-files/
-
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
+# Last.FM
 LASTFM_DEFAULT_USERNAME = 'kakdns'
 # Get these from http://www.last.fm/api/account
 LASTFM_API_KEY = ''
@@ -94,6 +77,6 @@ LASTFM_API_SECRET = ''
 
 # Local settings
 try:
-    from local_settings import *
+    from .local_settings import *
 except ImportError:
     pass
